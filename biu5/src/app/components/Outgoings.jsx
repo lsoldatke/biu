eac669eccfbd1986e073857c7acc91065f92f2aa
s24729-pj <s24729@pjwstk.edu.pl>
2025-05-23 17:07:23 +0200

    Add no items message [biu5]

--- biu5/src/app/components/Outgoings.jsx
+++ biu5/src/app/components/Outgoings.jsx
@@ -4,35 +4,39 @@ function Outgoings({ outgoings, onDelete, onSelect }) {
   return (
     <div id="outgoings">
       <h2>Outgoings</h2>
-      <table id="outgoings-table">
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th key={column}>{column}</th>
-            ))}
-            <th>actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {outgoings.map((outgoing) => (
-            <tr key={outgoing.id} onClick={() => onSelect(outgoing)}>
+      {outgoings.length > 0 ? (
+        <table id="outgoings-table">
+          <thead>
+            <tr>
               {columns.map((column) => (
-                <td key={column}>{outgoing[column]}</td>
+                <th key={column}>{column}</th>
               ))}
-              <td>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onDelete(outgoing.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
+              <th>actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {outgoings.map((outgoing) => (
+              <tr key={outgoing.id} onClick={() => onSelect(outgoing)}>
+                {columns.map((column) => (
+                  <td key={column}>{outgoing[column]}</td>
+                ))}
+                <td>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onDelete(outgoing.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No items</p>
+      )}
     </div>
   );
 }
